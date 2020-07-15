24d8791eb6745377f0c4aa749287fd91031c0bc8
hackey <hackey1318@naver.com>
2020-07-15 18:40:11 +0900

    change information security

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const config = require("./config/key")
+const bodyParser = require('body-parser')
+const {User} = require("./models/User")
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+// application/json
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://hackey:asdf1234@boilerplate.sfygn.mongodb.net/boilerplate?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURL,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(()=>console.log('MongoDB Connected...')).catch(err=>console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!!!!!'))
+app.get('/', (req, res) => res.send('Hello World_node.js!!'))
+
+app.post('/register', (req, res) => {
+    // register information -> get client
+    // into database
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
 
 app.listen(port, () => console.log(`Example app listening at ${port}`))
