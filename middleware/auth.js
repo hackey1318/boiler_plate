91ff0e5c914b2b63ea6c5481199c93c3202161fa
hackey <hackey1318@naver.com>
2020-07-20 16:11:38 +0900

    update logout part

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const {User}=require('../models/User')
 let auth = (req, res, next)=>{
     // process auth
     // get Token in client cookie
-    let token = req.cookie.x_auth;
+    let token = req.cookies.x_auth;
     //decrypt Token -> find user
     User.findByToken(token, (err, user)=>{
         if(err) throw err;
